825ef09af93df6493509ad0969aeee97f0f11fb4
jeznacki <jeznacki13@gmail.com>
2017-12-02 23:45:50 +0100

    Alertify registered for better UI infos

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ var siteCacheName = 'siteCacheNameV1'; //cache version - if modified cache will 
 var siteCachedFiles = [
 
     'css/app-shell.css',  //CSS
+    'https://cdnjs.cloudflare.com/ajax/libs/alertify.js/0.5.0/alertify.default.min.css',  //CSS
     './',
     'fonts/roboto/Roboto-Bold.woff',   //FONTS
     'fonts/roboto/Roboto-Bold.woff2',
@@ -20,12 +21,15 @@ var siteCachedFiles = [
     'fonts/roboto/Roboto-Thin.woff2',
     'js/init.js',  //JAVASCRIPT
     'js/vendor/materialize.js',
-    'js/vendor/jquery-3.2.1.min.js'
+    'js/vendor/jquery-3.2.1.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/alertify.js/0.5.0/alertify.min.js'
 
 ];
 
 //install event - when assets are cached
 self.addEventListener('install',function(ev){
+
+    //jjtodo: add alertify
     console.log('Service Worker: Instal event',ev);
 
     ev.waitUntil(
@@ -40,6 +44,7 @@ self.addEventListener('install',function(ev){
 
 //activate event - when cached assets are loaded
 self.addEventListener('activate',function(ev){
+    //jjtodo: add alertify
     console.log('Service Worker: Activate  event',ev);
 
     //jjtodo: cache referesh inmplementation needed
